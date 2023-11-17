36e4ff41e70b58c0cc25a50e203d2d370ac5cd9a
Trickster19 <harrismohd786@gmail.com>
2023-11-17 23:09:44 +0530

    payment and index update

--- middleware/index.js
+++ middleware/index.js
@@ -1,16 +1,16 @@
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const JwtDecoder=(req,res,next)=>{
-    try{
-    if (!req.headers.authorization) res.status(400).json({error:"token not found"});
-    const data=jwt.decode(req.headers.authorization);
-    req.user=data;
-    next();
+const JwtDecoder = (req, res, next) => {
+    try {
+        if (!req.headers.authorization) return res.status(400).json({ error: "token not found" });
+        const data = jwt.decode(req.headers.authorization);
+        req.user = data;
+        next();
     }
-    catch(error){
-        res.status(500).json({error});
+    catch (error) {
+        res.status(500).json({ error });
     }
 }
 
 
-module.exports=JwtDecoder;
+module.exports = JwtDecoder;
